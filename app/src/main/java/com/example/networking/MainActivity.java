6d3737da9944dcc0cf47397f7acb7f71beb279bf
Oscar <a18oscba@student.his.se>
2021-08-12 15:41:09 +0200

    Mountain
    Added an arraylist, arrayadapter and class for mountian

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.networking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<Mountain> mountainsList;
+    ArrayAdapter<Mountain> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mountainsList = new ArrayList<Mountain>();
+        adapter = new ArrayAdapter<Mountain>(this, android.R.layout.simple_list_item_1, mountainsList);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
     }
 }
